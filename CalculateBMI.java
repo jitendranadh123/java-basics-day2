6254f345d104d33867ec475cb516decc561984ff
jitendranadh123 <jitendranadhnaidu@gmail.com>
2025-12-22 10:04:29 +0530

    Add files via upload

---
+++ CalculateBMI.java
@@ -0,0 +1,8 @@
+public class CalculateBMI {
+    public static void main (String[] args){
+        double weight=70;
+        double height=1.75;
+        double bmi=weight/(height*height);
+        System.out.println("BMI:"+bmi);
+    }
+}
